08c12ef8912c5e03d482b2abec96202802104ca7
muhammadsohaib796 <sohaibshakeel796@gmail.com>
2026-07-15 03:11:59 +0500

    Update and get Api created

--- app/api/todo.py
+++ app/api/todo.py
@@ -10,3 +10,30 @@ router = APIRouter(prefix="/todo",tags={'Titan_todos'})
 @router.post('/',response_model=TodoResponse)
 def create(todo: TodoCreate, db: Session = Depends(get_db)):
     return create_todo(db,todo)
+
+
+@router.get('/')
+def get_all(db:Session = Depends(get_db)):
+    return get_todos(db)
+
+
+@router.get('/{todo_id}', response_model=TodoResponse)
+def get(todo_id:int, db:Session =Depends(get_db)):
+    todo = get_todo(db,todo_id)
+    if not todo:
+        raise HTTPException(
+            status_code=404,
+            detail="Todo Not Found"
+        )
+    
+    return todo
+
+
+@router.put('/{todo_id}')
+def update(todo_id:int, todo:TodoCreate, db:Session = Depends(get_db)):
+    updated = update(db,todo_id,todo)
+    if not updated:
+        raise HTTPException(
+            status_code=404,
+            detail= "Todo Not Found"
+        )
--- app/crud/todo.py
+++ app/crud/todo.py
@@ -13,3 +13,33 @@ def create_todo(db:Session,todo:TodoCreate):
     db.refresh(db_todo)
     return db_todo 
 
+def get_todos(db:Session):
+    return db.query(Todo).all()
+
+def get_todo(db:Session,todo_id:int):
+    query = db.query(Todo).filter(
+        Todo.id == todo_id
+    ).first()
+
+
+    return query
+
+def update_todo(
+        db:Session,
+        todo_id:int,
+        todo: TodoCreate
+):
+    todo_db = get_todo(db,todo_id)
+    if not todo_db:
+        return None
+    todo_db.title = todo.title
+    todo_db.description = todo.description
+
+    db.commit()
+    db.refresh(todo_db)
+
+    return todo_db
+
+
+    
+
